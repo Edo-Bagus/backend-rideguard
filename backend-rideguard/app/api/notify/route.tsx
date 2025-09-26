9df9d8451fbc693944fd147b9446152c3ff49265
EdoBagus <79005016+Edo-Bagus@users.noreply.github.com>
2025-09-26 21:11:37 +0700

    Use environment variables for Firebase credentials
    Replaces static import of serviceAccount with dynamic credential loading from environment variables for Firebase Admin SDK initialization. This improves security and flexibility for deployment.

--- backend-rideguard/app/api/notify/route.tsx
+++ backend-rideguard/app/api/notify/route.tsx
@@ -1,5 +1,4 @@
 import admin from "firebase-admin";
-import serviceAccount from "../../../firebase.json" assert { type: "json" };
 import { NextRequest } from "next/server";
 
 interface NotifyRequestBody {
@@ -8,14 +7,23 @@ interface NotifyRequestBody {
   body?: string;
 }
 
-
-// Inisialisasi Firebase Admin SDK (hanya sekali)
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: process.env.FIREBASE_AUTH_URI,
+      tokenUri: process.env.FIREBASE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+      clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+      universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    } as admin.ServiceAccount),
   });
 }
-
 
 export async function POST(req: NextRequest) {
   try {
